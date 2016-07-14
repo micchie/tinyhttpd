faa5e5e922db784f91eb5ecf398df8d67b407490
Michio Honda <micchie@sfc.wide.ad.jp>
2016-07-15 00:20:58 +0200

    add support for stackmap

--- Makefile
+++ Makefile
@@ -4,12 +4,18 @@ LDFLAGS += -lpthread -ldl # sqlite requires them
 EXTRA_CFLAGS += -I/usr/local/include -I../libsqlite/include
 EXTRA_LDFLAGS += ../libsqlite/lib/libsqlite3.a
 CFLAGS += $(EXTRA_CFLAGS)
-PROG = tinyhttpd
-OBJS = tinyhttpd.o
+PROG = tinyhttpd tinyhttpd-s
+OBJS = tinyhttpd.o tinyhttpd-s.o
 OPT = -DWITH_SQLITE
+SOPT = -DWITH_STACKMAP
+SPATH ?= -I../frankenstack -I../netmap/sys/ -DNETMAP_WITH_LIBS
 
 all: $(PROG)
 
+tinyhttpd-s: tinyhttpd-s.o
+	$(CC) $(CFLAGS) -o tinyhttpd-s tinyhttpd-s.o $(LDFLAGS) $(EXTRA_LDFLAGS)
+tinyhttpd-s.o: tinyhttpd.c
+	$(CC) $(CFLAGS) $(OPT) $(SOPT) $(SPATH) -c tinyhttpd.c -o tinyhttpd-s.o $(EXTRA_CFLAGS)
 tinyhttpd: tinyhttpd.o
 	$(CC) $(CFLAGS) -o tinyhttpd tinyhttpd.o $(LDFLAGS) $(EXTRA_LDFLAGS)
 tinyhttpd.o: tinyhttpd.c
--- tinyhttpd.c
+++ tinyhttpd.c
@@ -5,6 +5,11 @@
 #include <sys/stat.h>
 
 #include <sys/socket.h>
+#include <sys/mman.h>
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,6 +25,10 @@
 #ifdef WITH_SQLITE
 #include <sqlite3.h>
 #endif /* WITH_SQLITE */
+#ifdef WITH_STACKMAP
+#include <frankenstack.h>
+#include <frankenstack_user.h>
+#endif /* WITH_STACKMAP */
 
 struct dbinfo {
 	int	type;
@@ -28,23 +37,65 @@ struct dbinfo {
 		sqlite3 *sql_conn;
 		int	dumbfd;
 	};
+	int mmap;
+	char *paddr;
+	int cur;
+	union {
+		int maplen;
+		int maxlen;
+	};
+#define DBI_FLAGS_FDSYNC	0x1
+#define DBI_FLAGS_READPMEM	0x2
+	int flags;
+	char ifname[IFNAMSIZ+8]; /* stackmap ifname (also used as indicator) */
+#ifdef WITH_STACKMAP
+	struct fks_desc *fkd;
+#endif /* WITH_STACKMAP */
 } dbi;
 
 #define MAXCONNECTIONS 2048
 #define MAXQUERYLEN 65535
+#define MAXDUMBSIZE	204800
+#ifdef WITH_STACKMAP
+#define tcp_offset (sizeof(struct ethhdr) + sizeof(struct iphdr) \
+	+ sizeof(struct tcphdr)) + FKS_TCP_OPTLEN
+#define stackmap_offset (FKS_DMA_OFFSET + tcp_offset)
+static inline char *
+STACKMAP_BUF(struct netmap_ring *r, int i)
+{
+	return (NETMAP_BUF(r, i) + stackmap_offset);
+}
+
+static inline struct netmap_slot *
+STACKMAP_RX_NXT(struct netmap_ring *rxr, struct netmap_ring *exr, int fd, struct netmap_slot *hint)
+{
+	int i;
+
+	i = hint ? hint->next_idx : rxr->fdhead[fd];
+	if (i < 0)
+		return NULL;
+	else if (likely(i < rxr->num_slots))
+		return &rxr->slot[i];
+	return &exr->slot[i - rxr->num_slots];
+}
+#endif /* WITH_STACKMAP */
 
 enum { DT_NONE=0, DT_DUMB, DT_SQLITE};
 const char *SQLDBTABLE = "tinytable";
 
+#ifndef D
 #define D(fmt, ...) \
 	printf(""fmt"\n", ##__VA_ARGS__)
+#endif
 
 static int do_abort = 0;
+#if 0
 static u_int stat_nfds;
 static u_int stat_eps;
 static u_int stat_maxnfds;
 static u_int stat_minnfds;
 static uint64_t stat_vnfds;
+#endif /* 0 */
 
 void
 close_db(struct dbinfo *dbip)
@@ -55,6 +106,9 @@ close_db(struct dbinfo *dbip)
 
 	/* close reference */
 	if (dbip->type == DT_DUMB) {
+		if (dbip->mmap)
+			if (munmap(dbip->paddr, dbip->maplen))
+				perror("munmap");
 		D("closing dumbfd");
 		close(dbip->dumbfd);
 	}
@@ -164,16 +218,21 @@ int do_accept(int fd, int epfd)
 
 int do_established(int fd, ssize_t msglen, struct dbinfo *dbi)
 {
+	char buf[MAXQUERYLEN];
+	static u_int seq = 0;
 	ssize_t len;
-	static char buf[MAXQUERYLEN];
-	static int seq = 0;
+       
+	if (dbi->flags & DBI_FLAGS_READPMEM) {
+		len = dbi->mmap; /* page size */
+		goto direct;
+	}
 	len = read(fd, buf, sizeof(buf));
 
 	if (len == 0) {
 		close(fd);
 		return 0;
 	} else if (len < 0) {
-		D("%lu", len);
+		perror("read");
 		return -1;
 	}
 
@@ -181,15 +240,53 @@ int do_established(int fd, ssize_t msglen, struct dbinfo *dbi)
 		len = generate_httphdr(msglen, buf, NULL);
 	} else if (strncmp(buf, "POST ", strlen("POST ")) == 0) {
 		if (dbi->type == DT_DUMB) {
-			char query[MAXQUERYLEN];
-			/* create sql query for fair comparison with sqlite */
-			snprintf(query, sizeof(query),
-				"BEGIN TRANSACTION; insert into %s values (%d, '%s'); COMMIT;",
-			       	SQLDBTABLE, seq++, buf);
+direct:
+			if (dbi->mmap) {
+				char *p;
+				int d;
 
-			//len = write(dbi->dumbfd, buf, sizeof(len));
-			len = write(dbi->dumbfd, query, sizeof(len));
-			/* no need for fsync as O_SYNC */
+				d = (len & (dbi->mmap-1));
+				if (d)
+					len += dbi->mmap - d;
+				if (dbi->cur + len  > dbi->maplen)
+					dbi->cur = 0;
+				p = dbi->paddr + dbi->cur;
+				if (dbi->flags & DBI_FLAGS_READPMEM) {
+					int alen;
+					alen = read(fd, p, len);
+					if (alen < 0) {
+						perror("read");
+						return -1;
+					} else if (alen == 0) {
+						close(fd);
+						return 0;
+					}
+					if (strncmp(buf, "GET ", 
+					    strlen("GET ")) == 0)
+						goto gen_httpok;
+					else if (strncmp(p, "POST ",
+					         strlen("POST ")))
+						return -1; /* next pos stays */
+
+				} else
+					memcpy(p, buf, len);
+				if (msync(p, len, MS_SYNC))
+					perror("msync");
+				dbi->cur += len;
+			} else {
+				len = write(dbi->dumbfd, buf, len);
+				if (len < 0)
+					perror("write");
+				else if ((dbi->flags & DBI_FLAGS_FDSYNC ?
+				    fdatasync(dbi->dumbfd) :
+				    fsync(dbi->dumbfd)) < 0)
+					fprintf(stderr, "failed in f(data)sync\n");
+				dbi->cur += len;
+				if (dbi->cur > dbi->maxlen) {
+					lseek(dbi->dumbfd, 0, SEEK_SET);
+					dbi->cur = 0;
+				}
+			}
 		}
 #if WITH_SQLITE
 		else if (dbi->type == DT_SQLITE) {
@@ -209,9 +306,9 @@ int do_established(int fd, ssize_t msglen, struct dbinfo *dbi)
 				err_msg = NULL;
 				return -1;
 			}
-
 		}
 #endif /* SQLITE */
+gen_httpok:
 		len = generate_httphdr(msglen, buf, NULL);
 	}
 	write(fd, buf, len);
@@ -234,11 +331,17 @@ main(int argc, char **argv)
 #ifdef WITH_SQLITE
 	int ret = 0;
 #endif /* WITH_SQLITE */
+#ifdef WITH_STACKMAP
+	//struct fks_desc *stackmap_desc;
+	//struct nm_desc *ex_desc;
+#endif /* WITH_STACKMAP */
+	int i, nfd = 0; /* XXX initialize only to avoid warning  */
 
 	bzero(&dbi, sizeof(dbi));
 	dbi.type = DT_NONE;
+	dbi.maxlen = MAXDUMBSIZE;
 
-	while ((ch = getopt(argc, argv, "p:l:bd:")) != -1) {
+	while ((ch = getopt(argc, argv, "p:l:bmd:DNi:")) != -1) {
 		switch (ch) {
 		default:
 			D("bad option %c %s", ch, optarg);
@@ -267,6 +370,18 @@ main(int argc, char **argv)
 			dbi.path = optarg;
 			}
 			break;
+		case 'm':
+			dbi.mmap = getpagesize();
+			break;
+		case 'D':
+			dbi.flags |= DBI_FLAGS_FDSYNC;
+			break;
+		case 'N':
+			dbi.flags |= DBI_FLAGS_READPMEM;
+			break;
+		case 'i':
+			strncpy(dbi.ifname, optarg, sizeof(dbi.ifname));
+			break;
 		}
 
 	}
@@ -280,11 +395,34 @@ main(int argc, char **argv)
 	if (!port || !msglen)
 		usage();
 
+	if (dbi.flags & DBI_FLAGS_READPMEM) {
+		if (!((dbi.type == DT_DUMB) && dbi.mmap)) {
+			fprintf(stderr, "READPMEM must be used with dumb db and mmap\n");
+			usage();
+		}
+	}
 	if (dbi.type == DT_DUMB) {
-		dbi.dumbfd = open(dbi.path, O_RDWR | O_CREAT | O_SYNC, S_IRWXU);
+		dbi.dumbfd = open(dbi.path, O_RDWR | O_CREAT, S_IRWXU);
 		if (dbi.dumbfd < 0) {
 			perror("open");
 			goto close;
+		}
+		if (dbi.mmap) {
+			dbi.maplen = MAXDUMBSIZE; /* XXX is size ok? */
+			if (lseek(dbi.dumbfd, dbi.maplen - 1, SEEK_SET) < 0) {
+				perror("lseek");
+				goto close;
+			}
+			if (write(dbi.dumbfd, "", 1) != 1) {
+				perror("write");
+				goto close;
+			}
+			dbi.paddr = mmap(0, dbi.maplen, PROT_WRITE,
+				       	MAP_SHARED | MAP_FILE, dbi.dumbfd, 0);
+			if (dbi.paddr == MAP_FAILED) {
+				perror("mmap");
+				goto close;
+			}
 		}
 	}
 #ifdef WITH_SQLITE
@@ -293,6 +431,7 @@ main(int argc, char **argv)
 		char create_tbl_stmt[128];
 		char *journal_wal_stmt = "PRAGMA journal_mode = WAL";
 		char *excl_lock_stmt = "PRAGMA locking_mode = EXCLUSIVE";
+		char *synchronous_stmt = "PRAGMA synchronous = FULL";
 
 		/* open db file and get handle */
 		ret = sqlite3_open_v2(dbi.path, &dbi.sql_conn,
@@ -309,6 +448,11 @@ main(int argc, char **argv)
 			goto error;
 		/* avoiding shared memory cuts 4 us */
 		ret = sqlite3_exec(dbi.sql_conn, excl_lock_stmt,
+			       	NULL, NULL, &err_msg);
+		if (SQLITE_OK != ret)
+			goto error;
+		/* flush every commit onto the disk */
+		ret = sqlite3_exec(dbi.sql_conn, synchronous_stmt,
 			       	NULL, NULL, &err_msg);
 		if (SQLITE_OK != ret)
 			goto error;
@@ -365,21 +509,102 @@ error:
 		perror("listen");
 		goto close_socket;
 	}
+#ifdef WITH_STACKMAP
+	if (dbi.ifname[0]) {
+		dbi.fkd = fks_open(dbi.ifname);
+		if (!dbi.fkd) {
+			D("fks_open() failed");
+			goto close_socket;
+		}
+		fks_add_fd(dbi.fkd, sd);
+	} else
+#endif /* WITH_STACKMAP */
+	{
 
 	memset(&ev, 0, sizeof(ev));
 	ev.events = POLLIN;
 	ev.data.fd = sd;
 	epoll_ctl(epfd, EPOLL_CTL_ADD, sd, &ev);
+	}
 
 	signal(SIGINT, sig_h);
 	signal(SIGHUP, sig_h);
 
+#if 0
 	stat_eps = stat_nfds = stat_maxnfds = 0;
 	stat_vnfds = 0;
 	stat_minnfds = ~0;
+#endif /* 0 */
 	while (!do_abort) {
-		int i, nfd;
-		nfd = epoll_wait(epfd, evts, MAXCONNECTIONS, eptimeout);
+		/* fetch events */
+#ifdef WITH_STACKMAP
+		if (dbi.ifname[0]) {
+			struct netmap_ring *rxring, *txring, *exring;
+			struct fks_request fksr; /* XXX */
+			struct netmap_if *nifp = dbi.fkd->nmd->nifp;
+			struct netmap_if *enifp = dbi.fkd->exnmd->nifp;
+			struct netmap_slot *rxslot, *txslot;
+			u_int txcur, txlim, rxnum = 0;
+
+			rxring = NETMAP_RXRING(nifp, 0);
+			exring = NETMAP_RXRING(enifp, 0);
+			txring = NETMAP_TXRING(nifp, 0);
+
+			ioctl(dbi.fkd->fd, FKSIOCNSSYNC, &fksr);
+
+			txcur = txring->cur;
+			txlim = nm_ring_space(txring);
+
+			for (i = 0; i < rxring->nevt; i++) {
+				int fd;
+
+				fd = rxring->evts[i];
+				rxslot = STACKMAP_RX_NXT(rxring, exring, fd,
+							NULL);
+				txslot = &txring->slot[txcur];
+				while (rxslot) {
+					char *p;
+					struct netmap_slot *tmp;
+
+
+					p = STACKMAP_BUF(rxring, 
+							rxslot->buf_idx);
+					/* do something */
+					if (!strncmp(p, "POST ", strlen("POST "))) {
+						if (dbi->mmap)
+
+					}
+					if (!strncmp(p, "GET ", strlen("GET "))
+					    && txlim) {
+						char *d;
+						int len;
+
+						d = NETMAP_BUF(txring, 
+								txslot->buf_idx);
+						d += stackmap_offset;
+						len = generate_httphdr(msglen,
+							       	d, NULL);
+						txslot->len = len;
+						txslot->fd = fd;
+						txcur = nm_ring_next(txring, 
+								txcur);
+						txlim--;
+					}
+					tmp = rxslot;
+					rxslot = STACKMAP_RX_NXT(rxring, exring,
+							fd, rxslot);
+					tmp->next_idx = -1;
+					rxnum++;
+				}
+				rxring->fdhead[fd] = -1;
+			}
+			txring->cur = txcur;
+			rxring->head = rxring->cur = rxring->tail;
+			continue;
+		} else
+#endif /* WITH_STACKMAP */
+	       	{
+			nfd = epoll_wait(epfd, evts, MAXCONNECTIONS, eptimeout);
 #if 0
 		if (nfd > 0) {
 			stat_eps++;
@@ -399,7 +624,8 @@ error:
 			stat_minnfds = ~0;
 		}
 #endif /* 0 */
-
+		}
+		/* process events */
 		for (i = 0; i < nfd; i++) {
 			if (evts[i].data.fd == sd) {
 				if (do_accept(sd, epfd) < 0)
@@ -413,7 +639,12 @@ error:
 close_socket:
 	close(sd);
 close_ep:
-	close(epfd);
+	if (dbi.ifname[0])
+		close(epfd);
+#ifdef WITH_STACKMAP
+	else
+		fks_close(dbi.fkd);
+#endif /* WITH_STACKMAP */
 close:
 	close_db(&dbi);
 	return (0);
