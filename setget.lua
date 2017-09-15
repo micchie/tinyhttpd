733657275ca519baecb1563155853346870f4759
Michio Honda <micchie@sfc.wide.ad.jp>
2017-09-15 18:13:34 +0200

    setget.lua: set/get kvs-like traffic

---
+++ setget.lua
@@ -0,0 +1,30 @@
+local x = ""
+local y = ""
+local setlen = 1280
+local hlen = 8
+local t = setlen - hlen
+local p = 1000000
+local getlen = 64
+local t2 = getlen - hlen
+
+for i = 1, t2, 1 do
+	y = y.."y"
+end
+for i = 1, t, 1 do
+	x = x.."x"
+end
+request = function()
+	local n = math.random(p*2)
+
+	if n < p then
+		wrk.method = "POST"
+		wrk.path = "http://www.micchie.net/"
+		wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+		wrk.body = string.format("%-08d%s", n, x)
+	else
+		wrk.method = "GET"
+		wrk.path = string.format("/%-08d", n-p)
+		wrk.body = y
+	end
+	return wrk.format(wrk.method, wrk.path, wrk.headers, wrk.body)
+end
